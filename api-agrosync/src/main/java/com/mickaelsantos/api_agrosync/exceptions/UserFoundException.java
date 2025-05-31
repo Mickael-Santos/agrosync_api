e03273030346c736bbf2715d351ca1e728629ee1
Mickael-Santos <102236692+Mickael-Santos@users.noreply.github.com>
2025-05-30 21:41:09 -0400

    Delete api-agrosync directory

--- api-agrosync/src/main/java/com/mickaelsantos/api_agrosync/exceptions/UserFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mickaelsantos.api_agrosync.exceptions;
-
-public class UserFoundException extends RuntimeException {
-    public UserFoundException() {
-        super("Farm with this identification already exists");
-    }
-}
--- api-agrosync/src/main/java/com/mickaelsantos/api_agrosync/modules/user/dtos/CreateUserRequestDto.java
+++
@@ -1,7 +0,0 @@
-package com.mickaelsantos.api_agrosync.modules.user.dtos;
-
-public class CreateUserRequestDto {
-    private String username;
-    private String email;
-    private String password;
-}
